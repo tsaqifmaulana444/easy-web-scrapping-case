59c82cee4f28b79060cb15613bd13b3ddbd83e6e
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-01-27 10:55:21 +0700

    update

--- getTitle.py
+++ getTitle.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # you can insert the website link here
-url = "https://www.geeksforgeeks.org/"
+url = "https://docs.python.org/3/library/index.html"
 
 req = requests.get(url)
 soup = BeautifulSoup(req.content, "html.parser")
